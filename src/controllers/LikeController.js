275808cff901f0970a2cd30426e87016e7e58768
Lucas Calixto <lucas.calixto@eduzz.com>
2019-08-20 23:36:08 -0300

    Adiciona socket.io

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -2,6 +2,8 @@ const Dev = require('../models/Dev');
 
 module.exports = {
     async store(req, res) {
+        console.log(req.io, req.connectedUsers);
+
         const { user } = req.headers;
         const { devId } = req.params;
 
@@ -12,11 +14,6 @@ module.exports = {
             return res.status(400).json({error: 'Dev not exists!'});
         }
 
-        // Caso o alvo já tenha dado like no usuário atual, ocorre um match!
-        if (targetDev.likes.includes(loggedDev._id)) {
-            console.log('Deu Match!');
-        }
-
         // Impede que o usuário dê like 2x no mesmo dev
         if (loggedDev.likes.includes(targetDev._id)) {
             return res.json(loggedDev);
@@ -24,6 +21,20 @@ module.exports = {
 
         loggedDev.likes.push(targetDev._id);
         await loggedDev.save();
+
+        // Caso o alvo já tenha dado like no usuário atual, ocorre um match!
+        if (targetDev.likes.includes(loggedDev._id)) {
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            if (loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
+        }
 
         return res.json(loggedDev);
     }
--- src/server.js
+++ src/server.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://lucalix:lucalix@cluster0-j90wt.mongodb.net/tindev?retryWrites=true&w=majority', {
+const connectedUsers = {};;
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+
+    console.log(user, socket.id);
+
+    connectedUsers[user] = socket.id
+});
+
+mongoose.connect('url_login_password_mongo', {
     useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json()); 
-server.use(routes);
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
+
+app.use(cors());
+app.use(express.json()); 
+app.use(routes);
 
 server.listen(3333);
